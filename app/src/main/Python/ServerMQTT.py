a6ad74725c47acee7baaa82e3bf30cd7861bcb3f
fschirinzi34 <francesco.schirinzi1@studenti.unisalento.it>
2024-08-02 11:18:56 +0200

    Acquisizione dati con MQTT

--- app/src/main/Python/ServerMQTT.py
+++ app/src/main/Python/ServerMQTT.py
@@ -1,19 +1,32 @@
 import paho.mqtt.client as mqtt
 
+# MQTT settings
+BROKER_URL = "test.mosquitto.org"
+BROKER_PORT = 1883
+TOPICS = [("iot/accelerometer", 0), ("iot/gyroscope", 0)]
+
+# Callback when the client receives a message
+def on_message(client, userdata, message):
+    payload = message.payload.decode('utf-8')
+    print(f"Received message '{payload}' on topic '{message.topic}'")
+
+# Callback when the client connects to the broker
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("Connected successfully.")
+        print("Connected successfully")
+        # Subscribe to topics
+        for topic in TOPICS:
+            client.subscribe(topic)
     else:
-        print(f"Connection failed with code {rc}")
+        print(f"Connect failed with code {rc}")
 
-def on_message(client, userdata, msg):
-    data = msg.payload.decode('utf-8')
-    print(f"Received message: {data}")
-
+# Initialize MQTT client
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("127.0.0.1", 1883, 60)
-client.subscribe("iot/sensordata")
+# Connect to the MQTT broker
+client.connect(BROKER_URL, BROKER_PORT, 60)
+
+# Start the MQTT client loop
 client.loop_forever()
